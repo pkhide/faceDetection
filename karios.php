93cd4e9bf158247c0f4d41bdb00c9c857011a5d1
renovatio_ <desmorider@outlook.com>
2018-11-07 10:02:34 +0700

    add new API from karios(unsucessful)

---
+++ karios.php
@@ -0,0 +1,31 @@
+<?
+// set variables
+$queryUrl = "http://api.kairos.com/detect";
+$imageObject = '{"image":"https://media.kairos.com/liz.jpg"}';
+$APP_ID = "5f758a52";
+$APP_KEY = "c285c744c2892109755557dd91244b8a";
+
+$request = curl_init($queryUrl);
+
+
+// set curl optional
+curl_setopt($request, CURLOPT_POST, true);
+curl_setopt($request, CURLOPT_POSTFIELDS, $imageObject);
+curl_setopt($request, CURLOPT_HTTPHEADER, array(
+    "Content-type: application/json",
+    "app_id:" . $APP_ID,
+    "app_key:" . $APP_KEY
+    )
+);
+
+curl_setopt($request, CURLOPT_RETURNTRANSFER, true);
+$response = curl_exec($request);
+
+// show api response
+echo $response;
+
+// close session
+curl_close($request);
+
+
+?>
